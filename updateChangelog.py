20fd998c6a0781346fd047d2b3eb026cb1ef7e58
slaclau <seb.laclau@gmail.com>
2023-04-28 09:26:44 +0100

    Updat for focal

---
+++ updateChangelog.py
@@ -0,0 +1,7 @@
+import distro
+import fileinput
+
+codename = distro.codename()
+
+for line in fileinput.input("debian/changelog", inplace=True):
+    print(line.replace("replaceme", codename), end="")
